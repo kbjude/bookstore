4e668e3a73962f87718c4f7472fde242ad259b77
Jude Kajura <kbjude@gmail.com>
2021-02-22 19:55:52 +0300

    Adding listeners

--- src/Containers/booksForm.js
+++ src/Containers/booksForm.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 const BooksForm = () => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  
+  // const [title, setTitle] = React.useState('');
+  // const [category, setCategory] = React.useState('');
+
+  const handleChange = e => {
+    console.log(e.target.type);
+  };
+
   return (
     <form>
       <label htmlFor="title">
         Title:
-        <input name="title" placeholder="Title" type="text" id="title" />
+        <input name="title" placeholder="Title" type="text" id="title-id" onChange={handleChange} />
       </label>
       <select name="category">
         { categories.map(category => (
