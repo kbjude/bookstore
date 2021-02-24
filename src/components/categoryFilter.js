74ce75223bec126b5b226634d7238b705e8ecb1e
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-02-24 09:47:08 +0300

    UPDATE filter as soon as category is choosen without filter button

--- src/components/categoryFilter.js
+++ src/components/categoryFilter.js
@@ -3,20 +3,21 @@ import PropTypes from 'prop-types';
 
 const CategoryFilter = ({ filter }) => {
   const categories = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  const [category, setCategory] = React.useState(categories[0]);
   return (
     <div>
-      <select
-        name="category"
-        onChange={e => setCategory(e.target.value)}
-      >
-        {categories.map(category => (
-          <option key={category}>
-            {category}
-          </option>
-        ))}
-      </select>
-      <button type="button" onClick={() => filter(category)}> Filter </button>
+      <label htmlFor="category">
+        Choose Category:
+        <select
+          name="category"
+          onChange={e => filter(e.target.value)}
+        >
+          {categories.map(category => (
+            <option key={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </label>
       <br />
     </div>
   );
